22b54f82aae8263ff0dee176fecff5654ed53922
attilaf3 <fattila615@gmail.com>
2025-03-18 10:10:34 +0100

    Add optimize and instructions

--- ExcelReader.py
+++ ExcelReader.py
@@ -1,13 +1,17 @@
 import pandas as pd
-import os
 
+# Ctrl+Shift+Alt+L: code cleanup: hosszú sorok tördelése+importok optimalizálása
 # TODO:
 # input.csv-ból kiszedni a negyedórás adatokat:
 # PV termelését
 # Háztartás fogyasztását
 # Kiolvasni, dataframe-ben eltárolod
-#Commit1
-
-df = pd.read_csv('input.csv')
-df_filtered = df[~df.iloc[:, 0].astype(str).str.contains(":15|:30|:45", regex=True)]
+# Commit1
+# %%
+df = pd.read_csv('input.csv', sep=';', index_col=0, parse_dates=True)
+# df_filtered = df[~df.index.astype(str).str.contains(":15|:30|:45", regex=True)]  # df.resample("1h").first()
+df_filtered = df.resample("1h").sum()
+df_filtered.values  # numpy array
+# Optimize meghívása ezekkel az értékekkel
+# Valamilyen megjelenítése az erdményeknek
 print(df_filtered)
